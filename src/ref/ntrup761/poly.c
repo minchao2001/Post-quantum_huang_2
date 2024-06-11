2a9002f909285ed0cd802222d238a0f5a325781f
minchao2001 <b08204020@ntu.edu.tw>
2024-06-11 22:59:55 +0800

    Initial commit

--- src/ref/ntrup761/poly.c
+++ src/ref/ntrup761/poly.c
@@ -210,30 +210,3 @@ void fft_poly_mul(int16_t des[NTRUP_P], const int16_t src1[NTRUP_P], const int8_
     //free(b);
 }
 
-// void fft_poly_mul(int16_t *h, const int16_t *f, const int8_t *g)
-// {
-//     int16_t fg[NTRUP_P + NTRUP_P - 1];
-//     int16_t result;
-//     int i,j;
-
-//     for (i = 0; i < NTRUP_P; i++) {
-//       result = 0;
-//       for (j = 0;j <= i;++j) 
-//           result = cmod(result + f[j] * (int32_t)g[i - j], NTRUP_Q);
-//       fg[i] = result;
-//     }
-//     for (i = NTRUP_P; i < 2 * NTRUP_P - 1; ++i) {
-//       result = 0;
-//       for (j = i - NTRUP_P + 1; j < NTRUP_P; ++j) 
-//           result = cmod(result + f[j] * (int32_t)g[i - j], NTRUP_Q);
-//       fg[i] = result;
-//     }
-
-//     for (i = 2 * NTRUP_P - 2; i >= NTRUP_P; i--) {
-//       fg[i - NTRUP_P] = cmod(fg[i - NTRUP_P] + fg[i], NTRUP_Q);
-//       fg[i - NTRUP_P + 1] = cmod(fg[i - NTRUP_P + 1] + fg[i], NTRUP_Q);
-//     }
-
-//     for (i = 0; i < NTRUP_P; ++i) 
-//         h[i] = fg[i];
-// }
